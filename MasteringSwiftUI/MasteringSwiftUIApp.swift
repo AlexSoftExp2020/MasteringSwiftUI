14dda0694cc40c5817ff46575044b83072b77560
Alex <oleksii.pidhornyi@gmail.com>
2024-10-06 14:45:19 +0200

    Initial Commit

---
+++ MasteringSwiftUI/MasteringSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  MasteringSwiftUIApp.swift
+//  MasteringSwiftUI
+//
+//  Created by Oleksii on 06.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct MasteringSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
